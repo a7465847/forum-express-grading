7dd2cc30839fb74d6d52ff3dd6033d2c03da04f9
Song Shao Ting <qwe820610@gmail.com>
2021-02-01 13:21:55 +0800

    feat: Install passport and passport-local

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const db = require('./models')
 const app = express()
 const PORT = 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' })) 
 app.set('view engine', 'handlebars')
-app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
-app.use(flash())
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+
 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
@@ -22,6 +26,6 @@ app.listen(PORT, () => {
   console.log(`Express is running on http://localhost:${PORT}`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
 
 module.exports = app
