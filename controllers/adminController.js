3bcddcc2a3ebe601ebbb34f7f9dd195f83d469c3
Song Shao Ting <qwe820610@gmail.com>
2021-02-01 22:36:08 +0800

    feat: Add admin restaurant index page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,11 @@
-const adminController  = {
-    getRestaurants: (req, res) => {
-        return res.render('admin/restaurants')
-    }
+const db = require('../models')
+const Restaurant = db.Restaurant
+
+const adminController = {
+  getRestaurants: (req, res) => {
+    return Restaurant.findAll({ raw: true }).then(restaurants => {
+      return res.render('admin/restaurants', { restaurants })
+    })
   }
-  module.exports = adminController 
+}
+module.exports = adminController 
