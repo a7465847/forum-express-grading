86662845874fef550cb6b8b923779b8a6370376d
Song Shao Ting <qwe820610@gmail.com>
2021-02-08 14:20:48 +0800

    feat: Convert time format using moment

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -1,8 +1,13 @@
+const formatDistanceToNow = require('date-fns/formatDistanceToNow')
+
 module.exports = {
   ifCond: function (a, b, options) {
     if (a === b) {
       return options.fn(this)
     }
     return options.inverse(this)
+  },
+  moment: function (a) {
+    return formatDistanceToNow(a, {includeSeconds: true})
   }
 }
