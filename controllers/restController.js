43937ddf205264853131477d6271dd329eba5ecf
Song Shao Ting <qwe820610@gmail.com>
2021-02-18 09:53:12 +0800

    feat: Switch add/remove favorite button on restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -28,11 +28,11 @@ const restController = {
       const totalPage = Array.from({ length: pages }).map((_, index) => index + 1)
       const prev = page - 1 < 1 ? 1 : page - 1
       const next = page + 1 > pages ? pages : page + 1
-
       const data = result.rows.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),
-        categoryName: r.Category.name
+        categoryName: r.Category.name,
+        isFavorited: req.user.FavoritedRestaurants.map(d => d.id).includes(r.id)
       }))
       Category.findAll({
         raw: true,
